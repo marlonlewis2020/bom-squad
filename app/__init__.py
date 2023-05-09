e5d4f27ed88b24b1a274c54a20e884f8df521d4b
marlonlewis2020 <marlonlewis483@gmail.com>
2023-05-08 20:51:19 -0500

    updated init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, url_for, redirect, flash
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from app.config import env_object 
